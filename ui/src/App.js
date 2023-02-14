b5483f2ea56eccf4af4f94cf77efe89d670d80ba
Michael Wajand <wajmia18@htl-kaindorf.at>
2023-02-14 13:18:14 +0100

    gitignore

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,14 +1,14 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import CategoryList from "./CategoryList";
 import CategoryDescription from "./CategoryDescription";
-import Meal from "./Meal";
-import {useEffect, useState} from 'react';
+import MealList from "./MealList";
+import { useEffect, useState } from 'react';
 
 function App() {
     const [categories, setCategories] = useState([]);
     const [currentCategory, setCurrentCategory] = useState([]);
     let [categoryDesc] = useState([]);
-    let [currentMeal] = useState([]);
+    let [currentMeals, setCurrentMeals] = useState([]);
 
     useEffect(() => {
         fetch('http://localhost:8000/categories')
@@ -17,6 +17,10 @@ function App() {
                 setCategories(data.categories)
                 setCurrentCategory(data.categories[0].strCategoryDescription)
                 console.log(data.categories[0])
+            }))
+        fetch('http://localhost:8000/mealByName?name=' + "")
+            .then(res => res.json().then(data => {
+                setCurrentMeals(data.meals)
             }))
     }, []);
 
@@ -28,16 +32,23 @@ function App() {
         console.log(currentCategory)
         return currentCategory;
     }
+
     function searchForMeal(e) {
         console.log(e.target.value + ' ' + JSON.stringify(e.target.value))
         let searchString = e.target.value;
 
-        fetch('http://localhost:8000/mealByName?name='+searchString)
-        .then(res => res.json().then(data => {
-            //setCurrentMeal(data)
-        }))
+        fetch('http://localhost:8000/mealByName?name=' + searchString)
+            .then(res => res.json().then(data => {
+                if (data.meals != null) {
+                    setCurrentMeals(data.meals)
+                } else {
+                    const meals = [];
+                    setCurrentMeals(meals)
+                }
 
-        return currentMeal;
+            }))
+
+        return currentMeals;
     }
 
     // console.log(categories);
@@ -47,11 +58,13 @@ function App() {
             <h1>Meal-Finder</h1>
             <h3>Select a Category:</h3>
             <select value={currentCategory} onChange={(e) => handleChangeCategory(e)}
-                    id={"selectCategory"}><CategoryList categories={categories}/></select>
-            <CategoryDescription category={currentCategory}/>
+                id={"selectCategory"}><CategoryList categories={categories} /></select>
+            <CategoryDescription category={currentCategory} />
 
-            <input id="searchMeal" type="text" onChange={(e) => searchForMeal(e) }></input>
-            <Meal meal={currentMeal}></Meal>
+            <h3>Search a Meal:</h3>
+            <input id="searchMeal" type="text" onChange={(e) => searchForMeal(e)}></input>
+            <br />
+            <MealList meals={currentMeals} />
         </div>
     );
 
